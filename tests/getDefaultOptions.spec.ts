f7d349e218a875223f3d44f79f750c2af1c3d7a1
Jonasz Potoniec <jonasz@potoniec.eu>
2022-08-12 15:53:49 +0200

    added getDefaultOptions tests

---
+++ tests/getDefaultOptions.spec.ts
@@ -0,0 +1,39 @@
+import * as assert from "assert";
+import ipc from "node-ipc";
+import { IpcMode } from "../src/types/ipcMode";
+import { getDefaultOptions } from "../src/getDefaultOptions";
+
+describe("getDefaultOptions", () => {
+  it("should return default config on empty parameters", () => {
+    const options = {};
+    const expectedResult = {
+      ipcSocketId: "ipc-reporter",
+      ipcMode: IpcMode.client,
+      nodeIpcConfig: ipc.config,
+    };
+
+    assert.deepEqual(getDefaultOptions(options), expectedResult);
+  });
+
+  it("should not overwrite parameters", () => {
+    const options = { ipcSocketId: "test-reporter" };
+    const expectedResult = {
+      ipcSocketId: "test-reporter",
+      ipcMode: IpcMode.client,
+      nodeIpcConfig: ipc.config,
+    };
+
+    assert.deepEqual(getDefaultOptions(options), expectedResult);
+  });
+
+  it("should update node-ipc parameters", () => {
+    const options = { nodeIpcConfig: { id: "test" } };
+    const expectedResult = {
+      ipcSocketId: "ipc-reporter",
+      ipcMode: IpcMode.client,
+      nodeIpcConfig: { ...ipc.config, id: "test" },
+    };
+
+    assert.deepEqual(getDefaultOptions(options), expectedResult);
+  });
+});
